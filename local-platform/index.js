e7404735ab5627cf580f3202a4b198963d0dcb4e
CDao <iunknow@163.com>
2026-03-08 10:35:45 +0800

    chore(env): use only app/.env.local for local-platform

--- local-platform/index.js
+++ local-platform/index.js
@@ -21,14 +21,8 @@ function loadLocalEnv(filePath) {
   })
 }
 
-const LOCAL_PLATFORM_ENV = path.join(ROOT_DIR, '.env.local')
 const APP_ENV = path.join(ROOT_DIR, '../app/.env.local')
-
-if (fs.existsSync(LOCAL_PLATFORM_ENV)) {
-  loadLocalEnv(LOCAL_PLATFORM_ENV)
-} else {
-  loadLocalEnv(APP_ENV)
-}
+loadLocalEnv(APP_ENV)
 
 const app = express()
 const PORT = Number(process.env.PORT || 9000)
